e5eb672b968c45ad5d9e9602ded78395bbcce174
Anton Vassel <ant-vassel@ya.ru>
2022-04-14 11:04:28 +0300

    last commit

---
+++ HW_02_04_MVC/ViewControllers/WelcomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeViewController.swift
+//  HW_02_04_MVC
+//
+//  Created by Anton Vassel on 14.04.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var greetingLabel: UILabel!
+
+    var user: User!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        greetingLabel.text = "Greeting, \(user.person.firstName) \(user.person.surname)"
+    }
+}
