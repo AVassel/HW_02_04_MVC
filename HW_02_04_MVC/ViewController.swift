b1fcfef555eafd3927e32845a61608bb3ad87602
Anton Vassel <ant-vassel@ya.ru>
2022-04-14 09:54:32 +0300

    Initial Commit

---
+++ HW_02_04_MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_02_04_MVC
+//
+//  Created by Anton Vassel on 14.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
